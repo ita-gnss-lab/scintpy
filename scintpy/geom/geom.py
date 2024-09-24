a1fa4020f90f5d90363d2af96dd33ce21d4569c7
rodrigodelimaf <rdlfresearch@gmail.com>
2024-09-24 08:49:41 -0300

    test Rodrigo 2

--- scintpy/geom/geom.py
+++ scintpy/geom/geom.py
@@ -1,5 +1,5 @@
 def foo() -> None:
     number = input("What is your favourite number? ")
-    print("It is", number + 1)  # mypy test
+    print("It is", number + '1')  # mypy test
     x = "{} {}".format(1, 2)  # pyupgrade
     print(x)
