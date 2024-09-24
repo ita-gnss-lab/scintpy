966b06723a6b5e2de717bab0fc83ba18d81a282a
tapyu <rubem.engenharia@gmail.com>
2024-09-24 02:20:23 -0300

    update geom.py

--- scintpy/geom/geom.py
+++ scintpy/geom/geom.py
@@ -1,4 +1,5 @@
 def foo() -> None:
     number = input("What is your favourite number? ")
     print("It is", number + "1")  # mypy test
-    print("bar!")
+    x = "{} {}".format(1, 2)  # pyupgrade
+    print(x)
