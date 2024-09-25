2998716638495283838e9cd5c3b5b6ff71de0948
rodrigodelimaf <rdlfresearch@gmail.com>
2024-09-24 21:30:11 -0300

    tle data

---
+++ scintpy/main.py
@@ -0,0 +1,9 @@
+from geom.tleRequest import get_tle
+
+sat_id = "24876"
+date = "2023-09-10"
+username = "rdlfresearch@gmail.com"
+password = "dustrodrigo15304931"
+
+tle: str = get_tle(sat_id, date, username, password)
+print(tle)
